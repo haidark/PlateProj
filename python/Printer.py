ff4f941dd1a06aee7dc794bf0d34a6d19528040d
azer khan <azerkkhan@gmail.com>
2015-04-03 21:43:22 -0400

    Fixed Azer's mess
    The hard way

--- python/Printer.py
+++ python/Printer.py
@@ -29,59 +29,19 @@ def printPlate(P):
 	#checking if it is an instance of the Plate class 
 	if not (isinstance(P, Plate)):
 		print "Sorry, not a valid input"
-		#checking if it is an instance of the Plate class is sufficient
 	else: 
-
-		p1 = Plate(13.36, 100.0, 6)
-
-		#set data for well 1 =23
-		p1.setData (23, 0)
-		p1.setData (13, 1)
-		#set data for well 3 =43 		
-		p1.setData (43, 2)
-		#set data for well 2 =13
-		#set data for well 4 =5
-		p1.setData (5, 3)
-		#set data for well 5 =76
-		p1.setData (76, 4)
-		#set data for well 6 =54
-		p1.setData (54, 5)
-		#find out how many wells this plate has
-
-		numWells = p1.getNumberOfWells()
-		#TODO: get the number of wells in the plate
-			
-
-	#TODO: Print out the data as a single row of comma separated values
-	stringValue = str("")
-
 		# Get the number of wells in the plate
-	numWells = P.numWells
+		numWells = P.numWells
 		# Print out the data as a single row of comma separated values
 		# Initialize an empty string to build our string out of.
-	stringValue = ""
-
+		stringValue = ""
 
 		#for every well in this plate
-	for well in range(0, numWells):
-
-		stringValue = stringValue + str(p1.getWellValue(well)) + str(",")
-
-
-	
-	# Think of our plate (2x3) with 6 wells\
-	# letters indexing rows and numbers indexing columns then;
-	# Our data is in this order: A1, A2, A3, B1, B2, B3,
-	# NOT ths order: A1, B1, A2, B2, A3, B3
-
-
-	#You are done; to verify your results, make sure they match with the order \
-	#That I printed out earlier in this file.
-
-		 	# Build the string by adding the data from this well to it.
-
+		for well in range(0, numWells):
+			# Build the string by adding the data from this well to it.
+			stringValue = stringValue + str(P.data[well]) + str(", ")
 		#print the final constructed string
-	print stringValue
+		print stringValue
 
 #---------------------------------------------------
 # Now do it using your function (Dont touch this line of code either)
