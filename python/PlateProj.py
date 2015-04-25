1ca8d1a52dd5883ae0420d9e6e8c28ea1f272178
haidark <haidark@gmail.com>
2015-04-25 16:51:37 -0400

    Created final script

---
+++ python/PlateProj.py
@@ -0,0 +1,18 @@
+###################################################
+# Filename: Plate.py
+# Authors: Haidar Khan, Shuaib Peters, Azer Khan
+# Date: 3/24/15
+###################################################
+# Notes: (Anything after a '#' is a comment)
+# This file unifies the 3 modules for the Plate Project
+# 
+###################################################
+
+from Printer import printPlates
+from Parser import readXMLFile
+
+print "Reading the XML file."
+PlateList = readXMLFile(argv[1])
+print "Printing the CSV file..."
+printPlates(PlateList)
+print "Done!"
