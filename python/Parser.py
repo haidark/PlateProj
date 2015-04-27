8b8b43caa8f75a44e9e72c17143dc65fc21b7394
shupeters1 <shupeters1@gmail.com>
2015-04-27 17:59:22 -0400

    first dibs at assign 4
    import problem

--- python/Parser.py
+++ python/Parser.py
@@ -15,17 +15,24 @@ import xml.etree.ElementTree as ET
 tree = ET.parse('realData.xml')
 #we need to prepend everything witht he namespace in order to search for it...
 namespace = "{http://moleculardevices.com/microplateML}"
-#Gets root from test file
+#Gets root from real data file
 root = tree.getroot()
 
-def getWellXML(x, root):
+#def getWellXML(x, root):
 	# This function gets the (x-1)-th well from the XML file.
-	if int(x) > 96:
-		print "Your input is incorrect"
-	else:
-		wave = root[1][0].find(namespace+'microplateData').find(namespace+'wave')
-		print wave[x-1][0].find(namespace+'rawData').text
+#	if int(x) > 96:
+#		print "Your input is incorrect"
+#	else:
+#		wave = root[1][0].find(namespace+'microplateData').find(namespace+'wave')
+#		print wave[x-1][0].find(namespace+'rawData').text
 
-x = input("Please enter a number: ")
+
+
+def getWellXML(x, root):
+	x = input("Please enter the file name: ")
+	root = ET.fromstring()
+	print root
+
+
+#x = input("Please enter a number: ")
 getWellXML(x, root)
-	
