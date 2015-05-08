c73f11e55c6b0e3651dcc33a55533b31a1130076
azer khan <azerkkhan@gmail.com>
2015-05-07 22:51:45 -0400

    Azer
    Almost Done!

--- python/Printer.py
+++ python/Printer.py
@@ -9,9 +9,9 @@
 ###################################################
 #HK - DO NOT MODIFY THIS CODE:
 from Plate import Plate
-import random
+import random, string
 def makePlate():
-	#make a new plate, at time 13.37, temp 100 degrees, with 30 wells
+	#make a new plate, at time 13.37, temp 100 degrees, with 6 wells
 	plate = Plate(13.37, 100.0, 6)
 	#fill it with random numbers and print the desired output
 	for i in range(6):
@@ -40,16 +40,19 @@ def printPlate(P):
 	if not (isinstance(P, Plate)):
 		print "Sorry, not a valid input"
 	else: 
+		wellTemp = P.temp
+		wellTime = P.time
 		# Get the number of wells in the plate
 		numWells = P.numWells
 		# Print out the data as a single row of comma separated values
 		# Initialize an empty string to build our string out of.
-		stringValue = ""
+		stringValue = str(wellTemp) + ", " + str(wellTime) + ", " 
 
 		#for every well in this plate
 		for well in range(0, numWells):																										
 		# Build the string by adding the data from this well to it.
 			stringValue = stringValue + str(P.data[well]) + str(", ") 
+
 			
 		
 
@@ -64,9 +67,18 @@ def printPlate(P):
 
 def printPlates(Plates):	
 	WriteToCsvFile = open("test1.csv", "w")
+	WriteToCsvFile.write ("Temperature, Time")
+	#TODO get numRows and numCols from Plate Object
+	numRows = 2
+	numCols = 3
+
+	for letter in range(numRows):
+		for number in range(numCols):
+			WriteToCsvFile.write(", " + string.uppercase[letter] + str(number+1))
+	WriteToCsvFile.write("\n")
+
 	for Plate in Plates:
 		s = printPlate(Plate)
-
 		WriteToCsvFile.write (s+ "\n")
 	
 	WriteToCsvFile.close()
