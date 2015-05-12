54ae39f31b0e782760444aa4231373dec58a7089
shupeters1 <shupeters1@gmail.com>
2015-05-12 18:59:44 -0400

    assign 5

--- python/Parser.py
+++ python/Parser.py
@@ -12,22 +12,12 @@
 
 import xml.etree.ElementTree as ET
 
-#tree = ET.parse('realData.xml')
 #we need to prepend everything witht he namespace in order to search for it...
 namespace = "{http://moleculardevices.com/microplateML}"
-#Gets root from real data file
-#root = tree.getroot()
-
-#def getWellXML(x, root):
-	# This function gets the (x-1)-th well from the XML file.
-#	if int(x) > 96:
-#		print "Your input is incorrect"
-#	else:
-#		wave = root[1][0].find(namespace+'microplateData').find(namespace+'wave')
-#		print wave[x-1][0].find(namespace+'rawData').text
 
 
 a = raw_input("Please enter the file name: ")
+
 def getWellXML(a):
 	tree = ET.parse(a)
 	root = tree.getroot()
@@ -37,10 +27,14 @@ def getWellXML(a):
 		print child.tag
 		print child.text
 
-	#rawData = root[1][0][5][3][0].find(namespace+ 'oneDataSet')
-	#print rawData.tag
-	#print rawData.text
-	
 
-#x = input("Please enter a number: ")
 getWellXML(a)
+
+
+class Plate:
+	def __init__(self, temp, time, numWells):
+		self.numWells = numWells
+		self.temp = temp
+		self.time = time
+		self.data = [0 for x in range(numWells)]
+		self.wellLabels = [str(x) for x in range(numWells)]	
